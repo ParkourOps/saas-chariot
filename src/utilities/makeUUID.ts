938a2d767848ff07c0119eb69b534d8e01b4e8e6
Tej Birring <tejbirring@gmail.com>
2023-11-30 13:55:18 +0000

    Improve UUID generator function.

--- src/utilities/makeUUID.ts
+++ src/utilities/makeUUID.ts
@@ -1,5 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export default function() {
-    return uuidv4();
+export default function(specification?: { prefix?: string, suffix?: string }) {
+    let id = uuidv4();
+    if (specification?.prefix) {
+        id = specification.prefix + id;
+    }
+    if (specification?.suffix) {
+        id = id + specification.suffix;
+    }
+    return id;
 }
+
