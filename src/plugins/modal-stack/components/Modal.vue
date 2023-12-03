af2e25597b8bdc5b22fb47dd521bd47a40a68e18
Tej Birring <tejbirring@gmail.com>
2023-12-03 21:57:51 +0000

    Update modal.

--- src/plugins/modal-stack/components/Modal.vue
+++ src/plugins/modal-stack/components/Modal.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import makeUUID from '@/utilities/makeUUID';
-import { watch } from 'vue';
 import { ref } from 'vue';
 import { onMounted } from 'vue';
 
@@ -37,18 +34,9 @@ import { onMounted } from 'vue';
 </script>
 
 <template>
-    <dialog ref="dialog" class="modal">
-        <div class="modal-box w-11/12 max-w-5xl">
+    <dialog ref="dialog" class="modal modal-top sm:modal-middle">
+        <div class="modal-box overflow-visible">
             <slot :hideModal="hide" />
         </div>
     </dialog>
 </template>
-
-<!-- <h3 class="font-bold text-lg">Hello!</h3>
-<p class="py-4">Click the button below to close</p>
-<div class="modal-action">
-<form method="dialog">
-    
-    <button class="btn" @click="hide">Close</button>
-</form>
-</div> -->
