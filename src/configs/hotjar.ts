3aed5b33bf56ed9a6971907e0fbab37c996f3f94
Tej Birring <tejbirring@gmail.com>
2023-12-11 10:51:19 +0000

    Add Hotjar analytics.

---
+++ src/configs/hotjar.ts
@@ -0,0 +1,6 @@
+const hotjarConfigs = {
+    siteId: import.meta.env.VITE_HOTJAR_SITE_ID,
+    version: import.meta.env.VITE_HOTJAR_VERSION
+};
+
+export default hotjarConfigs;
--- src/libraries/use-analytics.ts
+++ src/libraries/use-analytics.ts
@@ -1,5 +1,7 @@
-import mixpanel from "mixpanel-browser";
+import hotjar from '@hotjar/browser';
+import mixpanel, { type Dict } from "mixpanel-browser";
 import mixpanelConfigs from "@/configs/mixpanel";
+import hotjarConfigs from '@/configs/hotjar';
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
@@ -7,10 +9,16 @@ export const useAnalytics = defineStore("Analytics", ()=>{
     const initialised = ref(false);
 
     function initialise() {
+        // initialise Mixpanel
         mixpanel.init(mixpanelConfigs.projectToken, {
             debug: import.meta.env.DEV,
             persistence: "localStorage"
         });
+        // initialise Hotjar
+        hotjar.init(hotjarConfigs.siteId, hotjarConfigs.version, {
+            debug: import.meta.env.DEV
+        });
+        // report success
         initialised.value = true;
         console.debug("Analytics initialised.");
     }
@@ -22,12 +30,20 @@ export const useAnalytics = defineStore("Analytics", ()=>{
 
         if (userId) {
             mixpanel.identify(userId);
+            hotjar.identify(userId, {});
         } else {
             mixpanel.reset();
+            hotjar.identify(null, {});
         }
     }
 
+    function trackEvent(eventName: string, properties?: Dict) {
+        mixpanel.track(eventName, properties);
+        hotjar.event(eventName);
+    }
+
     return {
-        identify
+        identify,
+        trackEvent
     }
 });
