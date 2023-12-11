fd96306dd0cc15f941a9ff788d361651896c8446
Tej Birring <tejbirring@gmail.com>
2023-12-11 11:17:02 +0000

    Add Tawk (tawk.to) messenger.

---
+++ src/configs/tawk.ts
@@ -0,0 +1,6 @@
+const tawkConfigs = {
+    propertyId: import.meta.env.VITE_TAWK_PROPERTY_ID,
+    widgetId: import.meta.env.VITE_TAWK_WIDGET_ID
+};
+
+export default tawkConfigs;
--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,13 @@ app.use(createPinia())
 app.use(router)
 app.use(modalStack)
 
+// add tawk.to (Tawk messenger)
+import TawkMessenger from '@tawk.to/tawk-messenger-vue-3';
+import tawkConfigs from './configs/tawk'
+app.use(TawkMessenger, {
+    propertyId: tawkConfigs.propertyId,
+    widgetId: tawkConfigs.widgetId
+})
+
+// mount
 app.mount('#app')
