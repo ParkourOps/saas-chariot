f62c6f4c2b6ea2abb40645d13b423e48b81fe187
Tej Birring <tejbirring@gmail.com>
2023-12-22 04:26:51 +0000

    Add typing for env vars.

--- env.d.ts
+++ env.d.ts
@@ -8,10 +8,17 @@ interface ImportMetaEnv {
     readonly VITE_FIREBASE_STORAGE_BUCKET: string
     readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
     readonly VITE_FIREBASE_APP_ID: string
+    readonly VITE_FIREBASE_USE_EMULATOR: string
+    // Analytics
     readonly VITE_FIREBASE_MEASUREMENT_ID: string
-    // Mixpanel]
     readonly VITE_MIXPANEL_PROJECT_TOKEN: string
-    // more env variables...
+    readonly VITE_HOTJAR_SITE_ID: string
+    readonly VITE_HOTJAR_VERSION: string
+    // User Feedback
+    readonly VITE_TAWK_PROPERTY_ID: string
+    readonly VITE_TAWK_WIDGET_ID: string
+    // Billing
+    readonly VITE_STRIPE_PUBLISHABLE_KEY: string
 }
   
 interface ImportMeta {
