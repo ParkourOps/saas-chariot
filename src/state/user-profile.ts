6e1efbb0d19fc9029f505220581594f53568d518
Tej Birring <tejbirring@gmail.com>
2023-12-28 18:45:47 +0000

    update model path.

--- src/state/user-profile.ts
+++ src/state/user-profile.ts
@@ -1,4 +1,4 @@
 import { subscribeToDoc } from "@/libraries/firebase/subscribe-to-doc";
-import UserProfile from "@m/UserProfile";
+import UserProfile from "@m/user/Profile";
 
 export const useUserProfile = subscribeToDoc("User Profile", UserProfile);
