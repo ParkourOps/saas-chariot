6e13cb29d3fdc647d784b8736c74174ceb5b0f99
Tej Birring <tejbirring@gmail.com>
2024-02-22 03:31:56 +0000

    Make modal encapsulate and execute action if provided. Update busy spinner props.

--- spa/src/components/modals/ModalAwaitAction.vue
+++ spa/src/components/modals/ModalAwaitAction.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+    import { onMounted } from "vue";
+    import { useIndicators } from "@/state/indicators";
 
-const props = defineProps<{
-    title?: string;
-    instruction: string;
-    mountAction?: () => void;
-}>();
+    const indicators = useIndicators();
 
-onMounted(props.mountAction ? props.mountAction : () => {});
+    const props = defineProps<{
+        title?: string;
+        instruction: string;
+        action?: (() => void | (()=>Promise<void>));
+    }>();
+
+    onMounted(async ()=>{
+        if (!props.action) {
+            return;
+        }
+        const token = indicators.registerPendingAction();
+        await props.action;
+        token.unregisterPendingAction();
+    });
 </script>
 
 <template>
@@ -19,7 +29,7 @@ onMounted(props.mountAction ? props.mountAction : () => {});
             <p class="text-center text-base-content/80" :class="[{ 'mt-2': !title }]">
                 {{ instruction }}
             </p>
-            <BusySpinner class="mx-auto mb-8 mt-14" />
+            <BusySpinner class="mx-auto mb-8 mt-14" colour="base-content/80" />
         </template>
     </Modal>
 </template>
