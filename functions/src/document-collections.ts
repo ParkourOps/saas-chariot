8ce690cd8e6569069acdce242621e6fcf22fdc11
Tej Birring <tejbirring@gmail.com>
2024-03-01 10:33:37 +0000

    Using mailing list document collection.

--- functions/src/document-collections.ts
+++ functions/src/document-collections.ts
@@ -2,7 +2,7 @@ import UserProfile from "@/_shared_/models/features/user-authentication/UserProf
 import {DocumentCollection} from "@/libraries/document-collection";
 import ActionLinkRecord from "./_shared_/models/features/action-link/ActionLinkRecord";
 import PurchasedItem from "./_shared_/models/features/billing/PurchasedItem";
-// import MailingListRecord from "./_shared_/models/features/mailing-list/MailingListRecord";
+import MailingListRecord from "./_shared_/models/features/mailing-list/MailingListRecord";
 // import DeliverableResourceRecord from "./_shared_/models/features/resource-delivery/DeliverableResourceRecord";
 
 export const customer = DocumentCollection.declare("customer/{}", UserProfile);
@@ -10,5 +10,5 @@ export const customerPurchase = DocumentCollection.declare("customer/{}/purchase
 
 export const actionLink = DocumentCollection.declare("action-link/{}", ActionLinkRecord);
 
-// export const mailingList = DocumentCollection.declare("mailing-list/{}", MailingListRecord);
+export const mailingList = DocumentCollection.declare("mailing-list/{}", MailingListRecord);
 // export const deliverableResource = DocumentCollection.declare("deliverable-resource/{}", DeliverableResourceRecord);
