f6bb68eac9f6438bbd08750f7387c44ae84aae45
Tej Birring <tejbirring@gmail.com>
2024-03-04 13:29:50 +0000

    Comment out unused params.

--- spa/src/router/index.ts
+++ spa/src/router/index.ts
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from "vue-router/auto";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    scrollBehavior(to /* from, savedPosition */) {
+    scrollBehavior(to/*, from, savedPosition*/) {
         if (to.hash) {
             // If anchored element, scroll to position.
             return {
