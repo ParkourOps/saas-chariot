3710f76133652bd6a1bf9cd7599a4272092392b7
Tej Birring <tejbirring@gmail.com>
2024-03-06 05:40:40 +0000

    Improve resource delivery API to allow 'not-found' to be returned (instead of throwing), and also to allow thumbnail URLs to be stored as detail of a deliverable resource.

--- functions/src/features/resource-delivery/data/deliverable-resources.ts
+++ functions/src/features/resource-delivery/data/deliverable-resources.ts
@@ -4,6 +4,9 @@ export default declareDeliverableResources({
     "xrbmvnmbqagf": {
         type: "cloud",
         path: "the-saas-feature-prioritisation-worksheet-v1.pdf",
-        title: "The SaaS Project Manager's Feature Prioritisation Worksheet (PDF)",
+        title: "The SaaS Product Manager's Feature Prioritisation Worksheet (PDF)",
+        thumbnails: [
+            "https://firebasestorage.googleapis.com/v0/b/saas-chariot.appspot.com/o/thumbnails%2F95da1798d9b55fa3e4a69c30c5e26830.jpg?alt=media&token=c04068bd-1735-4d78-8cfa-729407d8221f",
+        ],
     },
 });
--- functions/src/features/resource-delivery/get-resource-info.ts
+++ functions/src/features/resource-delivery/get-resource-info.ts
@@ -1,26 +1,34 @@
 import GetResourceInfo from "@/_shared_/models/features/resource-delivery/api/GetResourceInfo";
 import internalApiCall from "@/libraries/internal-api-call";
 import deliverableResources from "./data/deliverable-resources";
-import ServerlessFunctionError from "@/libraries/serverless-function-error";
 
 export default internalApiCall.implement(GetResourceInfo)(async (request)=>{
     const exists = Object.keys(deliverableResources).find((key)=>key === request.data.resourceKey);
     if (!exists) {
-        throw ServerlessFunctionError.create(
-            request.correlationId,
-            "not-found",
-            "Could not get resource information. Requested resource does not exist.",
-            {
-                inputVariables: {
-                    resourceKey: request.data.resourceKey,
-                },
-            }
-        );
+        return {
+            result: {
+                type: "not_found",
+            },
+        };
+        // throw ServerlessFunctionError.create(
+        //     request.correlationId,
+        //     "not-found",
+        //     "Could not get resource information. Requested resource does not exist.",
+        //     {
+        //         inputVariables: {
+        //             resourceKey: request.data.resourceKey,
+        //         },
+        //     }
+        // );
     } else {
         const resourceEntry = deliverableResources[request.data.resourceKey];
         return {
-            title: resourceEntry.title,
-            description: resourceEntry.description,
+            result: {
+                type: "found",
+                title: resourceEntry.title,
+                description: resourceEntry.description,
+                thumbnails: resourceEntry.thumbnails,
+            },
         };
     }
 });
