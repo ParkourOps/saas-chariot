960b945be5826c0cacb779816c939472cdd7d65b
Tej Birring <tejbirring@gmail.com>
2024-03-06 05:41:45 +0000

    Implement basic 404 page.

--- spa/src/pages/[...path].vue
+++ spa/src/pages/[...path].vue
@@ -1,3 +1,11 @@
 <template>
-    This is the fallback page (404).
+    <PagePaddedContent>
+        <div class="flex flex-col justify-center items-center">
+            <h1 class="text-2xl mb-8 text-center w-72 sm:w-auto font-light">
+                The resource you have requested does not exist.
+            </h1>
+            <Button variant="primary" size="lg" icon-left-class="fi fi-ss-home" :action="()=>$router.push({name:'/'})">Home</Button>
+        </div>
+        
+    </PagePaddedContent>
 </template>
