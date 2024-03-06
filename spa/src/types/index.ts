725bb83a5649c438e01df2836d6bdb4a8c37e0fd
Tej Birring <tejbirring@gmail.com>
2024-03-06 05:49:53 +0000

    Add types commonly used in components.

--- spa/src/types/index.ts
+++ spa/src/types/index.ts
@@ -1,8 +1,12 @@
 import type { additionalThemeColours } from "tailwind.config";
 import type { DaisyUiThemeColours } from "./theme-colours";
+import type { DefaultTheme } from "tailwindcss/types/generated/default-theme";
 
 export type NotificationType = "info" | "success" | "warning" | "error";
-export type ControlVariants = "neutral" | "primary" | "secondary" | "accent" | NotificationType;
-export type ThemeColours = DaisyUiThemeColours | keyof typeof additionalThemeColours;
+export type ControlVariant = "neutral" | "primary" | "secondary" | "accent" | NotificationType;
+export type ControlSize = "xs" | "sm" | "md" | "lg" | "xl";
+export type ScreenSize = keyof DefaultTheme["screens"];
+export type ThemeColours = keyof DaisyUiThemeColours | keyof typeof additionalThemeColours;
 export type HorizontalAlign = "left" | "centre" | "right";
 export type VerticalAlign = "top" | "centre" | "bottom";
+export type JustifyContent = "start" | "end" | "centre" | "normal" | "stretch" | "space-between" | "space-around" | "space-evenly";
