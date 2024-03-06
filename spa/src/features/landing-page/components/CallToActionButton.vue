7f18b180bf8e4bd4c916cf68ecbe34bc2e675f71
Tej Birring <tejbirring@gmail.com>
2024-03-06 14:55:06 +0000

    Turn main landing page CTA into a component.

---
+++ spa/src/features/landing-page/components/CallToActionButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    defineProps<{
+        price: string,
+    }>();
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <KeyboardButton
+            class="font-bold"
+            size="xl"
+            background-colour="accent"
+            foreground-colour="neutral"
+            :action="()=>$showModal(() => import('../components/modals/ModalJoinWaitingList.vue'), {
+                lockedInPrice: price,
+                leadMagnetResourceKey: 'xrbmvnmbqagf'
+            })"
+        >
+            <div class="flex items-center -mb-1 font-bold">
+                <p class="whitespace-nowrap mr-6">ACCESS NOW</p>
+                <SvgIcon name="diamond" class="h-12" />
+            </div>
+        </KeyboardButton>
+
+        <div class="px-4">
+            <p class="mx-auto mt-2 max-w-prose text-center font-serif text-primary font-semibold text-lg/snug sm:text-xl/tight">One-time purchase of <span class="font-bold">{{ price }}</span></p>
+            <p class="mx-auto max-w-prose text-center font-serif text-primary/60 text-base/none sm:text-lg/tight">(30-day money-back guarantee)</p>
+        </div>
+        
+    </div>
+</template>
