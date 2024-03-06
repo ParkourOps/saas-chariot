d2875c1945ab737551ef0401f64d78bc16a36b18
Tej Birring <tejbirring@gmail.com>
2024-03-06 14:34:39 +0000

    Add alert indicators.

---
+++ spa/src/components/indicators/StaticAlert.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { NotificationType } from '@/types';
+
+    defineProps<{
+        type: NotificationType;
+    }>();
+</script>
+
+<template>
+    <div 
+        class="p-6 rounded-box border-2 font-semibold text-center sm:text-left leading-tight"
+        :class="[
+            {'bg-info text-info-content border-info-content' : type === 'info' },
+            {'bg-success text-success-content border-success-content' : type === 'success' },
+            {'bg-warning text-warning-content border-warning-content' : type === 'warning' },
+            {'bg-error text-error-content border-error-content' : type === 'error' },
+        ]"
+    >
+        <slot />
+    </div>
+</template>
